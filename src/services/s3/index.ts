cdaf01031097305c4bc7ba4d4e9a20e411f8509a
Andrew Chen <andrew@jaanuu.com>
2026-01-31 17:55:10 -0800

    refactor: Remove unused S3 service files and interfaces

--- src/services/s3/index.ts
+++
@@ -1,9 +0,0 @@
-export { createS3Client } from './client';
-export { listObjects } from './listObjects';
-export type { ListObjectsParams } from './listObjects';
-export { uploadObject, createFolder } from './uploadObject';
-export type { UploadObjectParams, CreateFolderParams } from './uploadObject';
-export { getDownloadUrl, downloadObject } from './downloadObject';
-export type { DownloadObjectParams } from './downloadObject';
-export { deleteObject } from './deleteObject';
-export type { DeleteObjectParams } from './deleteObject';
